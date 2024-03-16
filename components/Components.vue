65e1bb4b7fde145e200b74a671fca42024c88eef
Eduardo <projecflex@gmail.com>
2024-03-16 01:47:56 -0300

    Update website and readme

--- components/Components.vue
+++ components/Components.vue
@@ -1,3 +1,42 @@
+<style scoped>
+    .alert.danger {
+    --tw-border-opacity: 1;
+    --tw-bg-opacity: .1;
+    background-color: #f43f5e1a;
+    background-color: rgb(244 63 94 / var(--tw-bg-opacity));
+    border-color: #f43f5e;
+    border-color: rgb(244 63 94 / var(--tw-border-opacity));
+}
+
+.alert.danger {
+    --tw-border-opacity: 1;
+    --tw-bg-opacity: 0.1;
+    background-color: rgba(244, 63, 94, .1);
+    background-color: rgb(244 63 94 / var(--tw-bg-opacity));
+    border-color: #f43f5e;
+    border-color: rgb(244 63 94 / var(--tw-border-opacity));
+}
+.p-4 {
+    padding: 1rem;
+}
+.border-l-4 {
+    border-left-width: 4px;
+}
+.rounded-bl-sm {
+    border-bottom-left-radius: 0.125rem;
+}
+.rounded-lg {
+    border-radius: 0.5rem;
+}
+.my-8 {
+    margin-bottom: 2rem;
+    margin-top: 2rem;
+}
+.relative {
+    position: relative;
+}
+</style>
+
 <template>
     <h1 style="text-align: center;font-size: medium;">Components</h1>
 
@@ -18,6 +57,7 @@
                     </div>
                 </div>
             </div>
+
 
 
             <div>
@@ -43,5 +83,23 @@ color: #5e5d5d;">Website</h3>
 
 
         </div>
+
+       
     </div>
+
+
+    <div class="relative p-4 border-l-4 my-8 rounded-lg rounded-bl-sm alert danger">
+            <div>
+                <h3>Can you help me?.</h3>
+                <p>I currently have no computer/laptop to update this project, due to financial situations. Currently I cannot/can't program, due to this difficulty.
+                </br>If you can help me, I promise that I will update this project again and do others, as I always have and always will.
+                
+                <br/> <br/>PayPal: eduh.castro19@gmail.com<br/><br/>
+
+        <b>If you can't, that's okay, you're already helping me a lot by accessing this site, and I'm very grateful for that.</b></p>
+    </div>
+    </div>
+
+
+   
 </template>
